a73b833b81b1a866f65ced32aaf0d4fd14fb3958
Aaron <avaartjes@assetscience.com>
2017-03-20 16:34:57 -0400

    Camera now has interesting movement. Also started work on spawner

---
+++ Assets/Scripts/MainCameraController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCameraController : MonoBehaviour
+{
+
+	public float displacementRatio = 0.02f;
+	public float minDistance = 0.2f;
+	public float maxDistance = 20f;
+
+	private Vector3 defaultPosition;
+
+	// Use this for initialization
+	void Start ()
+	{
+		defaultPosition = this.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		Vector3 mousePosition = GetInworldMousePosition ();
+		Vector3 targetDisplacement = ComputeTransformation (mousePosition);
+		this.transform.position = defaultPosition + targetDisplacement;
+	}
+
+	private Vector3 ComputeTransformation (Vector3 position)
+	{
+		return position *= displacementRatio;
+	}
+
+	private Vector3 GetInworldMousePosition ()
+	{
+		Vector2 mousePosition = Input.mousePosition;
+		Vector3 mouse3d = new Vector3 (mousePosition.x, mousePosition.y, this.transform.position.y);
+		return Camera.main.ScreenToWorldPoint (mouse3d);
+	}
+}
---
+++ Assets/Scripts/StressorGeneratorController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StressorGeneratorController : MonoBehaviour
+{
+
+	public float generatedRadius = 20f;
+
+	private static int generatedCount = 0;
+
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (shouldGenerate ()) {
+
+		}
+	}
+
+	private bool shouldGenerate ()
+	{
+		// For now just always return once a second
+		return Time.time > generatedCount;
+	}
+
+	private float getRandomAngle() {
+		return Random.Range (0, 359);
+	}
+}
