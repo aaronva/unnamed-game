55a5caf458420cd2a4006e37ffdb5bcbe5de7da2
Aaron <avaartjes@assetscience.com>
2017-05-28 20:40:09 -0400

    Main Menu navigation functional

--- Assets/Scripts/MainMenuController.cs
+++ Assets/Scripts/MainMenuController.cs
@@ -2,22 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MainMenuController : MonoBehaviour
 {
 	const int MainGameScene = 1;
 
+	public Slider difficultySlider;
+
 	public void LoadGrowthGame ()
 	{
 		GameManager.CurrentGameMode = GameManager.GameMode.Growth;
-//		GameManager.OverallDifficultyFactor = 0.5f + difficultyScaling;
+		GameManager.OverallDifficultyFactor = difficultySlider.value;
 		SceneManager.LoadScene (MainGameScene);
 	}
 
 	public void LoadSurvivalGame ()
 	{
 		GameManager.CurrentGameMode = GameManager.GameMode.Survival;
-//		GameManager.OverallDifficultyFactor = 0.5f + difficultyScaling;
+		GameManager.OverallDifficultyFactor = difficultySlider.value;
 		SceneManager.LoadScene (MainGameScene);
 	}
 
