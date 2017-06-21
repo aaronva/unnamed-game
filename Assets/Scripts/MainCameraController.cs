619c18915aee5e2414a317565f12d3b351c469ba
Aaron <avaartjes@assetscience.com>
2017-06-21 19:11:23 -0400

    Camera now tracks parent instead of mouse position

--- Assets/Scripts/MainCameraController.cs
+++ Assets/Scripts/MainCameraController.cs
@@ -5,47 +5,25 @@ using UnityEngine;
 public class MainCameraController : MonoBehaviour
 {
 
-	public float DisplacementRatio = 0.02f;
-	public float MinDistance = 0.2f;
-	public float MaxDistance = 20f;
+	public float DisplacementRatio = 0.4f;
+	public float AngularDisplacementRatio = 0.4f;
+	public Transform movingTarget;
 
 	private Vector3 defaultPosition;
-//	private Quaternion defaultRotation;
 
 	void Start ()
 	{
 		defaultPosition = this.transform.position;
-//		defaultRotation = this.transform.rotation;
 	}
 	
 	void Update ()
 	{
-		Vector3 mousePosition = GetInworldMousePosition ();
-		Vector3 targetDisplacement = ComputeNewPosition (mousePosition);
+		Vector3 targetDisplacement = ComputeNewPosition (movingTarget.position);
 		this.transform.position = defaultPosition + targetDisplacement;
-
-//		this.transform.ro
-//		Quaternion angularDelta = ComputeNewRotation(mousePosition);
-//		this.transform.rotation = angularDelta;
 	}
 
-	private Vector3 ComputeNewPosition (Vector3 mousePosition)
+	private Vector3 ComputeNewPosition (Vector3 lookingPosition)
 	{
-		return mousePosition * DisplacementRatio;
-	}
-
-	// TODO research and better understand Quaternion (and/or angles)
-	private Quaternion ComputeNewRotation (Vector3 mousePosition)
-	{
-//		Vector3 delta = this.transform.position - mousePosition;
-		return Quaternion.LookRotation (new Vector3(0,0,5) - this.transform.position, Vector3.forward);
-//		return Quaternion.FromToRotation (this.transform.position, delta);
-	}
-
-	private Vector3 GetInworldMousePosition ()
-	{
-		Vector2 mousePosition = Input.mousePosition;
-		Vector3 mouse3d = new Vector3 (mousePosition.x, mousePosition.y, this.transform.position.y);
-		return Camera.main.ScreenToWorldPoint (mouse3d);
+		return lookingPosition * DisplacementRatio;
 	}
 }
